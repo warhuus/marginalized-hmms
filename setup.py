afe90d2539cb4b15b9d60bf11d11ac6b3fe2dab2
Christopher Scott Warhuus <CHSWA@orsted.dk>
2021-01-20 15:39:36 +0100

    Fix setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
       name='mhmm',
-      version='1.0'
+      version='1.0',
       author='Christopher Warhuus',
       author_email='christopher.warhuus@gmail.com',
       packages=setuptools.find_packages(),
       install_requires=['torch',
                         'torchvision',
                         'numpy',
-                        'matplotlib']
+                        'matplotlib',
+                        'hmmlearn']
       )
